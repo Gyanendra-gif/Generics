bf43a7413f8b8be3cdb636ab6f8fa27c8e34a561
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-18 12:08:16 +0530

    [Gyanendra] Add: A Demo Program of Using Generics

---
+++ Generics Demo/Generics Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Generics_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+
+            Program.toPrint<int>(intArray);
+            Program.toPrint<double>(doubleArray);
+            Program.toPrint<char>(charArray);
+        }
+        public static void toPrint<T>(T[] intArray) 
+        {
+            foreach (var data in intArray)
+            {
+                Console.WriteLine(data);
+            }
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+}
+
